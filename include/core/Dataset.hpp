b11a0002c7ac4bcd278f3c002ae5dae2a6e4f1d3
Pau Romeu <romeu.pau@gmail.com>
2025-06-12 17:21:38 +0200

    feat: add matrix view const (bugged) and change naming

--- include/core/Dataset.hpp
+++ include/core/Dataset.hpp
@@ -1,24 +1,26 @@
 #include <iostream>
+#include "core/MatrixView.hpp"
 
 template <typename T = double>
 class Dataset {
   public:
     Dataset(std::size_t num_rows, std::size_t num_cols)
-        : data_(num_rows * num_cols), num_rows(num_rows), num_cols(num_cols) {
-        std::cout << num_rows << std::endl;
-        std::cout << num_cols << std::endl;
-    }
+        : data_(num_rows * num_cols), num_rows_(num_rows), num_cols_(num_cols) {}
 
     template <typename F>
     void fill(F &&visitor) {
-        for (std::size_t i = 0; i < num_rows; ++i) {
-            for (std::size_t j = 0; j < num_cols; ++j) {
-                data_[i * num_cols + j] = visitor(i, j);
+        for (std::size_t i = 0; i < num_rows_; ++i) {
+            for (std::size_t j = 0; j < num_cols_; ++j) {
+                data_[i * num_cols_ + j] = visitor(i, j);
             }
         }
     }
 
+    MatrixView<T> view() const {
+        return {data_.data(), num_rows_, num_cols_};
+    }
+
   private:
     std::vector<T> data_;
-    std::size_t num_rows, num_cols;
+    std::size_t num_rows_, num_cols_;
 };

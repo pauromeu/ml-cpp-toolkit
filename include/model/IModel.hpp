4274d66938eb054245ed4935661ed273f866eb2f
Pau Romeu <romeu.pau@gmail.com>
2025-06-12 15:37:29 +0200

    fix: asterisk for pointer in wrong position

--- include/model/IModel.hpp
+++ include/model/IModel.hpp
@@ -5,14 +5,14 @@ template <typename T = double>
 class IModel {
   public:
     virtual void fit(const MatrixView<T> &X, const std::vector<T> &y) = 0;
-    
+
     virtual T predict(const std::vector<T> &x) const = 0;
-    
+
     virtual std::vector<T> predictBatch(const std::vector<std::vector<T>> &X) const {
         std::vector<T> out;
         out.reserve(X.num_rows());
         for (std::size_t i = 0; i < X.num_rows(); i++) {
-            const T* row = X.data() + i * X.stride();
+            const T *row = X.data() + i * X.stride();
             out.emplace_back(predict({row, row + X.num_cols()}));
         }
         return out;
